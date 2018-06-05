c1b17a5faa3fb64ccbde95f34e25a195aa64f4fd
danielchisica <djimenez4223@universidadean.edu.co>
2018-06-05 00:21:16 -0500

    Implement CodeIgniter, and make an user entry into an event

--- formularioEvento.php
+++ formularioEvento.php
@@ -6,42 +6,28 @@
     <meta name="author" content="Andrey Niño">
     <meta name="author" content="Andres García">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-<meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="estilos.css">
     <title>Formulario Evento</title>
+    <style>
+        #map, #pan {
+            float: left;
+            height: 400px;
+            width: 400px;
+        }
+    </style>
 </head>
 <body>
     
     <div class="formularioEvento">
         <div class="form">
-            <h1>Crea un evento!!
-            
-            </h1>
+            <h1>Crea un evento!!</h1>
             
             <p><input type="date" name="fecha" id="fecha"></p>
             <p><input type="time" name="hora" id="hora"></p>
             <p><textarea name="descripcion" id="descripcion" cols="30" rows="10" placeholder="Descríbenos tu evento!"></textarea> </p>
             <p>¿Cuántas personas irán a tu evento?</p>
-            <p><select name="integrantes" id="integrantes">
-                <option value="0">Escoge una opción</option>
-                <option value="1">1</option>
-                <option value="2">2</option>
-                <option value="3">3</option>
-                <option value="4">4</option>
-                <option value="5">5</option>
-                <option value="6">6</option>
-                <option value="7">7</option>
-                <option value="8">8</option>
-                <option value="9">9</option>
-                <option value="10">10</option>
-                <option value="11">Hasta 20</option>
-                <option value="12">Hasta 30</option>
-                <option value="13">Hasta 50</option>
-                <option value="14">Hasta 80</option>
-                <option value="15">Hasta 100</option>
-                <option value="16">Más de 100</option>
-                <option value="17">Otra cantidad</option>        
-            </select>
-            </p>
+            <p><input type="text" name="integrantes" id="integrantes"></p>
             <p>¿Qué tipo de evento es?</p>
             <p>
                 <select name="tipoEvento" id="tipoEvento">
@@ -53,8 +39,18 @@
                     <option value="5">Cita</option>
                 </select>
             </p>
-        </div>        
+            <p>Ubica el banderín sobre el lugar y dale click para traer la ubicación</p>
+            <p><input type="text" name="latitude" id="lat">
+                <input type="text" name="longitude" id="lon"></p>
+            <div id="map"></div><div id="pan"></div>
+            <p>Selecciona una imagen que identifique tu evento(Opcional)</p>
+            <br/>
+            <input type="file" name="image" value="Examinar">
+            <br/><br/>
+            <input type="submit" name="send" value="Crear Evento">
+        </div>
     </div>
-    
+    <script src="JS/Maps.js"></script>
+    <script async defer src="https://maps.googleapis.com/maps/api/js?key=AIzaSyChTzXzJ1CWsrWn51ke7cXcu-2-fBqmy_I&callback=initMap"></script>
 </body>
 </html>
