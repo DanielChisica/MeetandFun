4237f49e26d7870defd06e06574d1f7bdc61d637
agarcia1907 <andresgf1907@hotmail.com>
2018-06-06 23:38:59 -0500

    agrego estilos

--- formularioUsuarios.php
+++ formularioUsuarios.php
@@ -2,51 +2,93 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
+    <meta charset="UTF-8">
     <meta name="author" content="Daniel Jiménez Chísica">
     <meta name="author" content="Andrey Niño">
     <meta name="author" content="Andres García">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-<meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">
+    <link rel="stylesheet" href="estilos.css">
+
     <title>Formulario Usuario</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 </head>
 <body>
    
-<!--   debe tener foto de perfil, nombre de usuario, biografía y redes sociales-->
-   <div class="formulario">
-      <!--Acá debería estar el nombre del usuario que debería salir de la base de datos.-->
-      
-      <h1 id="nombreUsuario" name="nombreUsuario"> ... </h1>
-      <textarea name="biografia" id="biografia" cols="30" rows="10" placeholder="Cuéntanos un poco más de ti"></textarea>
-      
-      
-      
-      <div class="redes">
-          <p>
-              Ingresa los URLs de tus redes sociales:
-          </p>
-          
-          <p>
-              <i class="fa fa-facebook-square" style="font-size:24px;color:blue"></i>
-              www.facebook.com/ 
-              <input type="text" name="usuarioFB" id="usuarioFB" placeholder="usuario Facebook">
-          </p>
-          
-          <p>
-              <i class="fa fa-instagram" style="font-size:24px;color:red"></i>
-              www.instagram.com/
-              <input type="text" name="usuarioIG" id="usuarioIG" placeholder="usuario Instagram">
-          </p>
-          
-          <p>
-             <i class="fa fa-twitter" style="font-size:24px;color:deepskyblue"></i>
-             www.twitter.com/
-             <input type="text" name="usuarioTW" id="usuarioTW" placeholder="usuario Twitter">
-          </p>
-          
-      </div>
-       
-   </div>
-    
+
+<section class="bg6 d-flex justify-content.center">
+    <div class="col-6 form">
+      <div class="registration-login-form">
+
+      <div class="tab-content ">
+					<div class="tab-pane active" id="home" role="tabpanel" data-mh="log-tab">
+						<div class="title h6 mt-3">Tu Cuenta </div>
+						<form class="content">
+							<div class="row">
+								<div class="col-lg-6 col-md-6">
+									<div class="form-group label-floating is-empty">
+										<label class="control-label">Nombre</label>
+										<input class="form-control" placeholder="" type="text">
+									</div>
+								</div>
+								<div class="col-lg-6 col-md-6">
+									<div class="form-group label-floating is-empty">
+										<label class="control-label">Apellido</label>
+										<input class="form-control" placeholder="" type="text">
+									</div>
+								</div>
+								<div class="col-xl-12 col-lg-12 col-md-12">
+									<div class="form-group label-floating is-empty">
+										<label class="control-label">Email</label>
+										<input class="form-control" placeholder="" type="email">
+									</div>
+									<div class="form-group label-floating is-empty">
+										<label class="control-label">Password</label>
+										<input class="form-control" placeholder="" type="password">
+									</div>
+
+									<div class="form-group date-time-picker label-floating">
+									<div class="input-group">
+                                        <span class="input-group-addon primary"><i class="fab fa-facebook-square icon2"></i></span>
+                                        <input type="text" class="form-control">
+                                    </div>
+
+                                    <div class="input-group">
+                                        <span class="input-group-addon primary"><i class="fab fa-twitter-square icon2"></i></span>
+                                        <input type="text" class="form-control">
+                                    </div>
+
+                                    <div class="input-group">
+                                        <span class="input-group-addon primary"><i class="fab fa-instagram icon2"></i></span>
+                                        <input type="text" class="form-control">
+                                    </div>
+                                    <div>
+
+                                    </div>
+
+									</div>
+									<div class="form-group label-floating is-select">
+										<label class="control-label">Genero</label>
+										<select class="selectpicker form-control" size="auto">
+											<option value="MA">Hombre</option>
+											<option value="FE">Mujer</option>
+											<option value="OT">Otro</option>
+										</select>
+									</div>
+
+									<a href="#" class="btn boton btn-lg full-width">Guardar!</a>
+								</div>
+							</div>
+						</form>
+					</div>
+
+				</div>
+
+    </div>
+</div>
+</section>
+
+<script defer src="https://use.fontawesome.com/releases/v5.0.13/js/all.js" integrity="sha384-xymdQtn1n3lH2wcu0qhcdaOpQwyoarkgLVxC/wZ5q7h9gHtxICrpcaSUfygqZGOe" crossorigin="anonymous"></script>
 </body>
 </html>
